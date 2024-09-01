e8cc3fa35ae17b36be9c47ec440e85e3babddc95
dplug1337 <{ID}+{username}@users.noreply.github.com>
2024-09-01 18:36:49 +0300

    added filteredStudents function

--- StudentSystem/main.c
+++ StudentSystem/main.c
@@ -6,14 +6,15 @@ typedef struct {
     int facultyNumber;
     char fullName[50];
     char specialty[11];
-    char birthday[11];
+    char birthdDate[11];
     int course;
     char gender;
     int grade;
 
 }Student;
 
-int CountStudentsInFile(const char* filename){
+int CountStudentsInFile(const char* filename)
+{
     FILE* file = fopen(filename, "r");
     if (!file){
         printf("Can't open the file: %s\n",filename);
@@ -30,23 +31,77 @@ int CountStudentsInFile(const char* filename){
     return count;
 }
 
+*Student readStudentsFromFile(const char* filename, int* studentCount)
+{
+    *studentCount = CountStudentsInFile(filename);
+    if (*studentCount <= 0)
+    {
+        printf("Can't open the file.")
+        return NULL;
+    }
+
+    Student* students = (Student*)malloc(studentCount*sizeof(Student));
+
+    if (!students)
+    {
+        printf("Insufficient memory");
+    }
+
+    FILE* file = fopen(filename,"r");
+
+    if (!file)
+    {
+        printf("Can't open the file");
+        free(students);
+        return NULL;
+    }
+
+
+    for(int i=0,i<*studentCount,i++)
+    {
+        fscanf(file, "%s %d %s %s %d %c %d",
+            students[i].name,
+           &students[i].facultyNumber,
+           students[i].specialty,
+           students[i].birthDate,
+           &students[i].course,
+           &students[i].gender,
+           &students[i].grade);
+    }
+    fclose(file);
+    return students;
+}
+
+*Student filteredStudents(*Student students, int* studentCount, const char* speciality, char gender, int birthYear, int* filderedCount )
+{
+    Student* filteredStudents = (Student*)malloc(studentCount*sizeof(Student));
+    if (!filteredStudents)
+    {
+        printf("Insufficient memory");
+        return NULL;
+    }
+
+    *filteredCount = 0;
+    for(int i=0; i<studentCount; i++)
+    {
+        sscanf(students[i].birthDate+6,"%d",&studentBirthYear);
+        if (strcmp(students[i].specialty, specialty) == 0 && students[i].gender == gender && studentBirthYear > birthYear) {
+            filteredStudents[(*filteredCount)++] = students[i];
+        }
+    }
+
+    if (*filteredCount == 0) {
+        free(filteredStudents);
+        return NULL;
+    }
+
+    return filteredStudents;
+
+}
+
 int main()
 {
-//    Student studenta = {501218049, "Vasil", "ITI", "14.08.1899", 2, 'M', 5};
-//    printf("Faculty number: %d\n", studenta.facultyNumber);
-//    printf("Fullname: %s\n", studenta.fullName);
-//    printf("Specialty: %s\n", studenta.specialty);
-//    printf("Birthday: %s\n", studenta.birthday);
-//    printf("Course: %d\n", studenta.course);
-//    printf("Gender: %c\n", studenta.gender);
-//    printf("Grade: %d\n", studenta.grade);
-//    return 0;
-    const char* filename = "students.txt";
-    int studentCount = CountStudentsInFile(filename);
 
-    if (studentCount != -1) {
-        printf("Current count in file %s is: %d\n", filename, studentCount);
-    }
 }
 
 
